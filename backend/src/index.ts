1cf80d759606a87555d625ac33e358ca7356941f
Arvind <150140351+Arvind05092005@users.noreply.github.com>
2025-02-07 05:59:09 +0530

    Initial commit

---
+++ backend/src/index.ts
@@ -0,0 +1,27 @@
+import { Hono } from "hono";
+
+const app = new Hono();
+
+app.post("/api/v1/user/signup", (c) => {
+  return c.text("Hello Hono!");
+});
+app.post("/api/v1/user/signin", (c) => {
+  return c.text("Hello Hono!");
+});
+
+app.post("/api/v1/user/blog", (c) => {
+  return c.text("Hello Hono!");
+});
+
+app.put("/api/v1/user/blog", (c) => {
+  return c.text("Hello Hono!");
+});
+
+app.get("/api/v1/user/blog", (c) => {
+  return c.text("Hello Hono!");
+});
+app.get("/api/v1/user/blog/blog", (c) => {
+  return c.text("Hello Hono!");
+});
+
+export default app;
