b04fac8fe9669d9b0b762fb6d22b5a1a1bc70fa9
Arvind <150140351+Arvind05092005@users.noreply.github.com>
2025-02-08 12:16:22 +0530

    updated common folder

--- common/src/zod.ts
+++ common/src/zod.ts
@@ -1,3 +1,5 @@
+import { title } from "process";
+import { PassThrough } from "stream";
 import z from "zod";
 export const signupInput = z.object({
   username: z.string().email(),
@@ -5,3 +7,14 @@ export const signupInput = z.object({
   name: z.string().optional(),
 });
 export type SignupInput = z.infer<typeof signupInput>;
+export const signinInput = z.object({
+  username: z.string().email(),
+  password: z.string().min(6),
+});
+export type signininput = z.infer<typeof signinInput>;
+export const createblogInput = z.object({
+  title: z.string(),
+  content: z.string(),
+});
+
+export type createblogInput = z.infer<typeof createblogInput>;
